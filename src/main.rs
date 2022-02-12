dc3f7cf01a8f7d34cc99cf6ffbb062e8f280210e
Letheed <letheed-dev@outlook.com>
2022-02-12 18:01:54 +0100

    Migrate from `clap` 2.x to `clap` 3.0.x

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 
 use std::{result::Result as StdResult, time::Duration};
 
-use anyhow::{anyhow, Context};
+use anyhow::Context;
 use clap::ArgMatches;
 use rgol::World;
 
@@ -20,10 +20,10 @@ MAPS:
 type Result<T = ()> = anyhow::Result<T>;
 
 fn main() -> Result {
-    use clap::{clap_app, crate_authors, crate_version};
+    #[allow(deprecated)]
+    use clap::{clap_app, crate_authors, crate_description, crate_version};
 
-    #[allow(clippy::needless_pass_by_value)]
-    fn is_number(s: String) -> StdResult<(), String> {
+    fn is_number(s: &str) -> StdResult<(), String> {
         if s.chars().all(|c| c.is_digit(10)) {
             Ok(())
         } else {
@@ -31,23 +31,20 @@ fn main() -> Result {
         }
     }
 
+    #[allow(deprecated)]
     let matches = clap_app!(rgol =>
-        (about: "Conway's game of life for terminal in Rust")
         (author: crate_authors!())
         (version: crate_version!())
+        (about: crate_description!())
         (after_help: MAPS_MSG)
-        (@setting ColoredHelp)
         (@setting SubcommandRequiredElseHelp)
-        (@setting VersionlessSubcommands)
         (@subcommand genmap =>
             (about: "Prints an empty map")
-            (@setting ColoredHelp)
             (@arg NROW: {is_number} * "Number of rows")
             (@arg NCOL: {is_number} * "Number of columns")
             (@arg space: -s --space "Adds spaces to the map"))
         (@subcommand play =>
             (about: "Plays the game (CTRL-c to exit)")
-            (@setting ColoredHelp)
             (@arg FILE: * "File containing the map")
             (@arg TICK_MS: {is_number} "Elapsed time between iterations in ms")
         )
@@ -55,26 +52,18 @@ fn main() -> Result {
     .get_matches();
 
     match matches.subcommand() {
-        ("genmap", Some(args)) => genmap(args),
-        ("play", Some(args)) => play(args),
-        _ => anyhow::bail!("subcommands failed to match properly"),
+        Some(("genmap", args)) => genmap(args),
+        Some(("play", args)) => play(args),
+        _ => unreachable!("SubcommandRequiredElseHelp prevents `None`"),
     }
 }
 
 /// "genmap" subcommand.
 ///
 /// Prints an empty map.
-fn genmap(args: &ArgMatches<'_>) -> Result {
-    let nrow = args
-        .value_of("NROW")
-        .ok_or_else(|| anyhow!("NROW has no value"))?
-        .parse()
-        .context("NROW is not a number")?;
-    let ncol = args
-        .value_of("NCOL")
-        .ok_or_else(|| anyhow!("NCOL has no value"))?
-        .parse()
-        .context("NCOL is not a number")?;
+fn genmap(args: &ArgMatches) -> Result {
+    let nrow: usize = args.value_of_t("NROW")?;
+    let ncol: usize = args.value_of_t("NCOL")?;
     if nrow == 0 || ncol == 0 {
         return Ok(());
     }
@@ -93,21 +82,19 @@ fn genmap(args: &ArgMatches<'_>) -> Result {
 
 /// "play" subcommand.
 ///
-/// Loads a map from a file and seeds the world with it, then plays it.
-fn play(args: &ArgMatches<'_>) -> Result {
-    let filename = args.value_of("FILE").ok_or_else(|| anyhow!("FILE has no value"))?;
-    let tick_ms = args
-        .value_of("TICK_MS")
-        .map_or(Some(TICK_MS), |t_ms| t_ms.parse().ok())
-        .ok_or_else(|| anyhow!("TICK_MS is not a number"))?;
-    let tick = Duration::from_millis(tick_ms);
-    let world = load_world(filename).with_context(|| format!("in {}", filename))?;
-    play_world(world, tick)
-}
+/// Loads a map from a file and plays it.
+fn play(args: &ArgMatches) -> Result {
+    use clap::ErrorKind;
 
-/// Loads and parses the world from a file.
-fn load_world(filename: &str) -> Result<World> {
-    Ok(std::fs::read_to_string(filename)?.parse()?)
+    let filename = args.value_of("FILE").expect("FILE is required");
+    let tick_ms = match args.value_of_t("TICK_MS") {
+        Err(error) if error.kind() == ErrorKind::ArgumentNotFound => Ok(TICK_MS),
+        result => result,
+    }?;
+    let tick = Duration::from_millis(tick_ms);
+    let world = { || -> Result<_> { Ok(std::fs::read_to_string(filename)?.parse()?) } }()
+        .with_context(|| format!("in {}", filename))?;
+    play_world(world, tick)
 }
 
 /// Plays the world.
